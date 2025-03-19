0cd067cc119ca31f603a0d5fdbabe8128246ffed
Eduardo Tadeu <eduardo.tsilva@hotmail.com>
2025-03-19 17:18:29 -0400

    refactor(projeto): Alterado o nome do pacote principal do projeto e adicionado os mais utilizados

--- src/main/java/com/seven/cors/chat/api/ChatApiApplication.java
+++ src/main/java/com/seven/cors/chat/api/ChatApiApplication.java
@@ -1,4 +1,4 @@
-package com.seven.cors.chat_api;
+package com.seven.cors.chat.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
